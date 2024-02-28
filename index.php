84e144a98646519e6bc71c0fcef7bbb7bf0105d1
nandanabila <161547865+nandanabila@users.noreply.github.com>
2024-02-28 10:59:05 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/index.css">
+    <title>galeri foto nabila</title>
+</head>
+<body>
+     <main>
+        <div class="Container">
+            <h2>Selamat Datang</h2>
+            <div class="InnerContainer">
+                <a href="login.php" class="Login">Login</a>
+                <a href="register.php" class="Daftar">Daftar</a>
+            </div>
+        </div>
+        <div class="Banner">
+            <h1>Website Galeri Foto</h1>
+        </div>
+    </main>
+    <script src="asset/js/script.js"></script>
+</body>
+</html>
+   
+
+
